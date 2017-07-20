43eba094191b34018a082e85316e11588fd729fc
cmsegui <cmsegui@yahoo.com>
2017-07-19 22:04:34 -0400

    set up hbs views

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -7,6 +7,27 @@ var app     = express();
 // assigning 3000 as our port
 var port    = 3000;
 
+var hbs = require('hbs');
+
+app.set("view engine", "hbs");
+app.set('views', './views');
+
+
+
+
+app.get('/', function (require, response) {
+    response.send("Welcome to Pizza Express!");
+});
+
+app.get('/topping/:type', function(require, response, next) {
+    response.send("pepperoni pizza! Good choice.");
+});
+
+app.get('/order/:amount/:size', function(require, response, next) {
+
+     response.send("Your order for 10 medium pizzas will be ready in 1 minute!");
+ });
+
 // tells the server to listen for requests on port 3000
 app.listen(port, function(){
   console.log("==========================")
