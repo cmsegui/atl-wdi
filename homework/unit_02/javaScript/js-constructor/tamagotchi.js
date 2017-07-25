5645dcb3df834277342b42afe0c6f883d2b61233
cmsegui <cmsegui@yahoo.com>
2017-07-24 23:51:38 -0400

    part 5 added new methods

--- homework/unit_02/javaScript/js-constructor/tamagotchi.js
+++ homework/unit_02/javaScript/js-constructor/tamagotchi.js
@@ -20,16 +20,32 @@ class Tamagotchi {
         this.restedness--;
         console.log(this.name + ' has a current restedness of: ' + this.restedness);
     }
-
+    start() {
+        this.hungerTimer = setInterval(function() {
+            this.cry();
+        }, 6000); 
+        this.yawnTimer = setInterval(function() {
+            this.cry();
+        }, 10000); 
+        this.sickTimer = setInterval(function) {
+            this.cry();
+        }, 20000);
+    }
+    stop() {
+        clearInterval(this.hungerTimer);
+        clearInterval(this.yawnTimer);
+        clearInterval(this.sickTimer);
+    }
 }
 
-
+function add(a, b) {
+    return a + b;
+}
+let sum = add(3,3);
 //create new Tamagotchis
 let t1 = new Tamagotchi("Lulu", "Dinosaur");
 let t2 = new Tamagotchi("Patty", "Chicken");
 
-t1.puke();
-t2.yawn();
 
 
 
