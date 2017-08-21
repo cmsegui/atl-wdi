5d862aa9be167805038c59130c0956ff0f86d8fc
cmsegui <cmsegui@yahoo.com>
2017-08-21 16:48:56 -0400

    Nil Skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -1,6 +1,20 @@
-a = true
-b = false
-a = b.equal?(a)
-    #puts a
-a == b.equal?(a) 
-    puts a
+#   a = true
+
+#   b = false
+
+#   a = b.equal?(a)
+#       puts a
+
+
+#   a == b.equal?(a) 
+#       puts a
+
+#   number = nil
+
+#   puts number 
+
+#   puts "#{number}nil was here"
+
+#   print number
+
+#   puts number.class
